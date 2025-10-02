0a9a27776142b30f5f5f55ebb57561e7146ad24b
Girolex <alejandrogiron72@gmail.com>
2025-10-02 11:33:09 -0400

    updated log in and added Registration page

--- blog project folder/Flask_code/ OG_blog.py
+++ blog project folder/Flask_code/ OG_blog.py
@@ -1,8 +1,16 @@
-from flask import Flask, redirect , url_for , render_template , request
+from flask import Flask, flash, redirect , url_for , render_template , request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, validators
+
 # create a flask app instace 
 app = Flask(__name__)
+app.secret_key = "random_shite"
 
-
+class RegistrationForm(FlaskForm):
+    name = StringField('name',[validators.DataRequired(), validators.length(min = 2, max = 24)])
+    email = StringField('email',[validators.DataRequired(), validators.length(min=6, max = 35)])
+    password = PasswordField('password',[validators.DataRequired()])
+    confirm_password = PasswordField('confirm_password', validators=[validators.EqualTo('password', message = 'password must match')])
 
 # Define a route and a view function
 @app.route("/")
@@ -10,14 +18,21 @@ def home():
     return render_template("index.html")
 
 # this is the login
-@app.route("/login", methods = ["POST" , "GET"])
+@app.route("/login", methods = ["GET" , "POST"])
 def login():
     return render_template("login.html")
 
 # this leads you to the registration page
-@app.route ("/register")
-def regiter():
-    return render_template("Register.html",  methods = ["POST" , "GET"])
+@app.route ("/register", methods = ["GET" , "POST"])
+def register():
+    form = RegistrationForm()
+    if    form.validate_on_submit() : 
+        
+        flash('registration succesfull')
+        print(form.errors)
+        return redirect(url_for('login'))
+    
+    return render_template("Register.html", form = form )
 
 #displays whatever you type in the URL
 @app.route("/<name>")
